282ab9480df1c50e01fe676a1294964e07916d63
Daniel Farina <daniel@fdr.io>
2016-11-07 23:43:04 -0800

    Add "body" field
    This will hold the text of a todo item.
    
    Because we modified a migration in-place, which is an expedient, it's
    necessary to re-migrate the database from scratch, by running `bundle
    exec rake db:setup` again.

--- db/migrate/1478587789_create_todos.rb
+++ db/migrate/1478587789_create_todos.rb
@@ -4,6 +4,7 @@ Sequel.migration do
       uuid         :id, default: Sequel.function(:uuid_generate_v4), primary_key: true
       timestamptz  :created_at, default: Sequel.function(:now), null: false
       timestamptz  :updated_at, default: Sequel.function(:now), null: false
+      text         :body, null: false
     end
   end
 end
